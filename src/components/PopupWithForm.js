08d21dd86219a53ce030440f018e619ec334a0d3
Andrey Varlamov <hadzime@yandex.ru>
2020-10-24 15:56:50 +0300

    откат

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -2,19 +2,10 @@ import React from 'react';
 
 
 const PopupWithForm = React.memo(({ isOpen, onClose, title, name, buttonText, children, onSubmit }) => {
-  // Реф для ссылки на валидируемую форму
-  const formRef = React.useRef();
-
-
-  // React.useEffect(() => {
-  //   const test = new FormValidator(formClassNames, formRef.current);
-  //   test.enableValidation();
-  // }, [])
-
-
+ 
   return (
     <section className={`popup popup_${name} ${isOpen ? 'popup_opened' : ''}`}>
-      <form action="#" method="POST" className="popup__container" noValidate onSubmit={onSubmit} ref={formRef}>
+      <form action="#" method="POST" className="popup__container" noValidate onSubmit={onSubmit}>
         <button type="reset" aria-label="Закрыть" className="popup__close-btn" onClick={onClose}></button>
         <h2 className="popup__name">
           {title}
